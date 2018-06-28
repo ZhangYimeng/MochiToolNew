47cc375c5a79bc862044ade500aaf4eed7711015
Zhang Yimeng <saitoiyoli@sina.com>
2018-06-28 09:37:23 +0800

    20180628

---
+++ src/main/java/person/mochi/tool/integer/MochiInteger.java
@@ -0,0 +1,35 @@
+package person.mochi.tool.integer;
+
+public class MochiInteger {
+
+	private Long integer;
+	
+	public MochiInteger() {
+		integer = new Long(0);
+	}
+	
+	public MochiInteger(long num) {
+		integer = new Long(num);
+	}
+	
+	public Long getInteger() {
+		return integer;
+	}
+	
+	public void addUp(long num) {
+		integer += num;
+	}
+	
+	public void increase() {
+		integer++;
+	}
+	
+	public void multiply(long num) {
+		integer *= num;
+	}
+	
+	public void makeDouble() {
+		integer *= 2;
+	}
+	
+}
