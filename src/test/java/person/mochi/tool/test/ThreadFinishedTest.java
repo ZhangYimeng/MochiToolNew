3f95a0bb017785a2f2a72c898037e735c5ec0236
Zhang Yimeng <saitoiyoli@sina.com>
2018-08-24 15:44:33 +0800

    20180824

---
+++ src/test/java/person/mochi/tool/test/ThreadFinishedTest.java
@@ -0,0 +1,30 @@
+package person.mochi.tool.test;
+
+public class ThreadFinishedTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread t = new Thread(new SelfRunnable());
+		t.start();
+		while(true) {
+			Thread.sleep(500);
+			System.out.println(t.isAlive());
+		}
+	}
+
+	private static class SelfRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			for(int i = 0; i < 20; i++) {
+				try {
+					Thread.sleep(500);
+					System.out.println("正在训练中…………");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+}
